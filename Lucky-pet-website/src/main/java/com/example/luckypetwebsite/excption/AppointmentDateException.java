bbc35f66be2764eb608c23899150783fc8ad5775
Amaal Alzhrani <71808558+myhopesx@users.noreply.github.com>
2022-06-12 10:08:05 +0300

    Delete Lucky-pet-website directory

--- Lucky-pet-website/src/main/java/com/example/luckypetwebsite/excption/AppointmentDateException.java
+++
@@ -1,7 +0,0 @@
-package com.example.luckypetwebsite.excption;
-
-public class AppointmentDateException extends RuntimeException{
-    public AppointmentDateException(String message) {
-        super(message);
-    }
-}
--- Lucky-pet-website/src/main/java/com/example/luckypetwebsite/excption/InvalidIdException.java
+++
@@ -1,8 +0,0 @@
-package com.example.luckypetwebsite.excption;
-
-public class InvalidIdException extends RuntimeException {
-    public InvalidIdException(String message) {
-        super(message);
-    }
-
-}
